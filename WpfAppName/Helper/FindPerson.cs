2bfe5a194932bce4a64a7ce41a931d5d727c4a49
Nikita <grishchenkonikita@mail.ru>
2021-12-18 21:44:36 +0300

    upload data

---
+++ WpfAppName/Helper/FindPerson.cs
@@ -0,0 +1,22 @@
+﻿using loboratornay.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loboratornay.Helper
+{
+    class FindPerson
+    {
+        int id;
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+        public bool PersonPredicate(Person role)
+        {
+            return role.Id == id;
+        }
+    }
+}
